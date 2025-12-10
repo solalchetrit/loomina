51099c541dbdf28503595fc8dda41ef1f9661404
Sotchee <solal@MacBook-Pro-de-Solal.local>
2025-12-10 15:58:04 +0100

    fix: restore layout and explicitly define favicon in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,10 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  icons: {
+    icon: '/icon.png',
+    apple: '/icon.png',
+  },
 };
 
 export default function RootLayout({
