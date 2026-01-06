0d64ad5ddac862bc76b4bdb141e095ee9d328cb5
solalchetrit <solalchetrit@icloud.com>
2026-01-06 15:52:05 +0100

    chore: update verification webhook URL to real Make scenario

--- website/config/make.ts
+++ website/config/make.ts
@@ -1,3 +1,3 @@
 export const MAKE_CONFIG = {
-    VERIFY_WEBHOOK_URL: "https://hook.eu1.make.com/FIXME_VERIFY_WEBHOOK",
+    VERIFY_WEBHOOK_URL: "https://hook.eu1.make.com/riqogz4ys1696le1mii2wawu7bc8ok2h",
 };
