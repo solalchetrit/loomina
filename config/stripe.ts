2de0bfa9fb650c7dde3fc72668b4daea51cc3c2a
Solal Chetrit <sotchee@mac-1.home>
2026-01-20 15:49:33 +0100

    fix(stripe): update payment link to new 449 EUR product

--- config/stripe.ts
+++ config/stripe.ts
@@ -1,4 +1,4 @@
 export const STRIPE_CONFIG = {
     // Lien de paiement officiel Loomina
-    PAYMENT_LINK: "https://buy.stripe.com/eVqfZi1Jh5Rn6R7dhM7Zu00",
+    PAYMENT_LINK: "https://buy.stripe.com/4gM3cwafN4NjgrH4Lg7Zu01",
 };
